5427144bad2fbb4ae19bb904ae91ee455591d84b
Ava-TU <C24384981@myTUDublin.ie>
2026-07-08 00:33:11 +0100

    fixing up falling objects

--- TU984_GameJam/Assets/Scripts/ObjectFallController.cs
+++ TU984_GameJam/Assets/Scripts/ObjectFallController.cs
@@ -3,17 +3,26 @@ using UnityEngine;
 public class ObjectFallController : MonoBehaviour
 {
 
-    float wait = 0.8f;
+    public float wait = 0.8f;
+    public float targetTime;
+
     public GameObject fallingObject;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        InvokeRepeating("Fall", wait, wait);
+        //InvokeRepeating("Fall", wait, wait);
+        targetTime = wait;
     }
 
-    void Fall()
+    void Update()
     {
-        Instantiate(fallingObject, new Vector3(Random.Range(-10, 10), 10, 0), Quaternion.identity);
+        targetTime -= Time.deltaTime;
+
+        if (targetTime <= 0.0f)
+        {
+            Instantiate(fallingObject, new Vector3(Random.Range(-10, 10), 10, 0), Quaternion.identity);
+            targetTime = wait;
+        }
     }
 }
