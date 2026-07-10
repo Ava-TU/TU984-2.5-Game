8e9b52155cdd427227c2743afd04da4196a21925
Ava-TU <C24384981@myTUDublin.ie>
2026-07-10 18:27:50 +0100

    save

--- TU984_GameJam/Assets/Scripts/GameManager.cs
+++ TU984_GameJam/Assets/Scripts/GameManager.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     //public PlayerController player;
-    //public MainTimer game;
+    public MainTimer game;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
